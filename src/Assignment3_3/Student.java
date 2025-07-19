a0d0f855cb9daca1e92a23dba17f7e8a0fe32b54
Tebrawy <boodytebry@gmail.com>
2025-07-19 17:30:21 +0300

    Assignment array 2 Student class

--- src/Assignment3_3/Student.java
+++ src/Assignment3_3/Student.java
@@ -11,17 +11,51 @@ public class Student {
         this.gpa = gpa;
     }
 
-    void displayinfo(){
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getGpa() {
+        return gpa;
+    }
+
+    public void setGpa(double gpa) {
+        this.gpa = gpa;
+    }
+
+    public void displayinfo(){
         System.out.println(name + " is "+ age + " years old, and his gpa is "+ gpa);
     }
-    void study(){
-        gpa+= 0.5;
+    public void study(){
+        if (gpa < 4)
+            gpa += 0.1;
+        else
+            System.out.println("You have maximum gpa");
     }
 
     public static void main(String[] args) {
         Student Ahmed = new Student("Ahmed",19,3.4);
         Student Marwan = new Student("Marwan",21,2.5);
+        Ahmed.getGpa();
+        Ahmed.getAge();
+        Ahmed.getName();
+        Marwan.setAge(28);
+        Marwan.setName("Tebry");
+        Marwan.setGpa(4);
         Marwan.displayinfo();
+        Ahmed.study();
         Ahmed.displayinfo();
         Marwan.study();
         Marwan.displayinfo();
